62e35495910d89f293c201ad82440cb01ed2f52a
Liviu Dobre <liviu.dobre@xogito.com>
2019-11-01 17:10:34 +0200

    Fix url register tradesman

--- mon/traders/urls.py
+++ mon/traders/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('create', views.TradesmanSaveView.as_view() ,name='create-tradesman')
+    url('create', views.TradesmanSaveView.as_view(), name='create-tradesman')
 
 ]
